e9808bf4faf037c95f8a6ab5539f8f911aabbce1
Marek <keraml@gmail.com>
2012-04-18 23:32:46 +0200

    beginning of presentation and styles

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,16 @@
+$(function() {
+    var html = $('html');
+    var w = $(window);
+
+    if (!Modernizr.input.placeholder) {
+        html.addClass('no-placeholder');
+    }
+
+    $("#presentation").slides({
+        generateNextPrev: true,
+        pagination: false,
+        generatePagination: false
+    });
+
+});
+
