ffc6f5fcafc48d4a56cc93cf66c551db24330bcb
Marek <keraml@gmail.com>
2012-04-20 14:02:06 +0200

    optimize images

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -5,12 +5,12 @@ $(function() {
     if (!Modernizr.input.placeholder) {
         html.addClass('no-placeholder');
     }
+//
+//    $("#presentation").slides({
+//        generateNextPrev: true,
+//        pagination: false,
+//        generatePagination: false
+//    });
 
-    $("#presentation").slides({
-        generateNextPrev: true,
-        pagination: false,
-        generatePagination: false
-    });
-  
 });
 
